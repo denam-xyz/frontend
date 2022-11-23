3b9e85febf5ec4fb369a3e992dbbd523122c602c
thomas779 <thomasehumphreys@gmail.com>
2022-11-23 21:12:57 +0100

    change server address

--- src/services/NetworkService.js
+++ src/services/NetworkService.js
@@ -1,4 +1,4 @@
-const serverAddress = "http://localhost:3000/";
+const serverAddress = "https://backend-s9tj.onrender.com/";
 if (process.env.REACT_APP_API_URL) {
   serverAddress = process.env.REACT_APP_API_URL;
 }
