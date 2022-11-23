0b39ee4979a16c123c98127d639e61fb67b93c2b
thomas779 <thomasehumphreys@gmail.com>
2022-11-23 23:22:41 +0100

    add serverAddress

--- src/services/NetworkService.js
+++ src/services/NetworkService.js
@@ -1,4 +1,4 @@
-const serverAddress = "https://backend-s9tj.onrender.com/";
+const serverAddress = "http://localhost:3000/" || "https://www.denam.xyz/";
 if (process.env.REACT_APP_API_URL) {
   serverAddress = process.env.REACT_APP_API_URL;
 }
